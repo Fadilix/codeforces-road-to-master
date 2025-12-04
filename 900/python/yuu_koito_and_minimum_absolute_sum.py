57c1044e9a83b3068b12f7fd1b11729f745e829b
Fadilix <iamfadile2@gmail.com>
2025-12-04 01:29:08 +0100

    feat: solved Yuu Koito and Minimum Absolute Sum

---
+++ 900/python/yuu_koito_and_minimum_absolute_sum.py
@@ -0,0 +1,55 @@
+t = int(input())
+
+
+for _ in range(t):
+    n = int(input())
+    a = list(map(int, input().split()))
+
+    # min_val = float("inf")
+    # print(min_val)
+
+    # b = []
+    b = []
+
+
+    # print("This is b", *b)
+    # print(abs(sum(b)))
+
+    if a[0] == -1 and a[-1] != -1:
+        a[0] = a[-1]
+
+    elif a[0] != -1 and a[-1] == -1:
+        a[-1] = a[0]
+
+    elif a[0] == -1 and a[-1] == -1:
+        a[-1] = a[0] = 0
+
+    for i in range(len(a)):
+        if a[i] == -1:
+            a[i] = 0
+    
+    for i in range(len(a) -1):
+        b.append(a[i + 1] - a[i])
+
+    print(abs(sum(b)))
+    print(*a)
+
+
+    # print(min(b, key=lambda x: x >= 0))
+    # print("This is the final", *a)
+    
+    # for i in range(len(a) - 1):
+    # if a
+    # b.append(a[i + 1] - a[i])
+
+    # print(sum(b))
+
+
+# a = list(map(int, input().split()))
+
+# s = 0
+
+# for i in range(len(a) - 1):
+#     s += a[i + 1] - a[i]
+
+# print(s)
